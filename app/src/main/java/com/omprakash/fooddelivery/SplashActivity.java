d6463942e7c4136c24ee4a8bafa5da5f6ea75c38
Omprakash <omprakash.mopuri@improve10x.com>
2023-03-14 15:01:04 +0530

    Implemented Splash Activity

--- app/src/main/java/com/omprakash/fooddelivery/SplashActivity.java
+++ app/src/main/java/com/omprakash/fooddelivery/SplashActivity.java
@@ -2,13 +2,27 @@ package com.omprakash.fooddelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.omprakash.fooddelivery.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, DeliveryActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
     }
 }
