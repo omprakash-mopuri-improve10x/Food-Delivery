768a0df7781c66a53c68ef036ca766f31ba24340
Omprakash <omprakash.mopuri@improve10x.com>
2023-03-14 16:29:13 +0530

    Implemented delivery item design

--- app/src/main/java/com/omprakash/fooddelivery/DeliveryActivity.java
+++ app/src/main/java/com/omprakash/fooddelivery/DeliveryActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.omprakash.fooddelivery.databinding.ActivityDeliveryBinding;
+
 public class DeliveryActivity extends AppCompatActivity {
+
+    private ActivityDeliveryBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_delivery);
+        binding = ActivityDeliveryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
